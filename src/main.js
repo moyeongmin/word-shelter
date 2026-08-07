61a7c7b6f6c5f7853a2034d19cdd08f94a7f7fb0
moyeongmin <qa98589858@gmail.com>
2026-08-07 19:00:43 +0900

    fix: .gitignore 적용 및 불필요한 파일 캐시 제거

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import BaseCampScene from './scenes/BaseCampScene.js';
 import ForestScene from './scenes/ForestScene.js';
-import DesertScene from './scenes/DesertScene.js';
+import NorthForestScene from './scenes/NorthForestScene.js';
 
 import './ui/style.css';
 
@@ -15,11 +15,11 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 0 },
-            debug: false
+            debug: false,
         }
     },
     // BaseCampScene이 배열의 첫 번째이므로 게임 시작 시 가장 먼저 실행됨
-    scene: [BaseCampScene, ForestScene, DesertScene] 
+    scene: [BaseCampScene, ForestScene, NorthForestScene] 
 };
 
 const game = new Phaser.Game(config);
